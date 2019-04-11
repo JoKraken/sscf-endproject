42a74b021148e3195b6f90af705fc881a8f9af35
JoKrakan <J.Kraken24@hotmail.com>
2019-04-11 15:15:00 +0300

    add login frontend

---
+++ front/js/login.js
@@ -0,0 +1,53 @@
+app.controller('loginCtrl', function($scope) {
+
+    console.log(localStorage.login);
+    if (localStorage.login == "true") {
+        document.querySelector('.errorLogin').style.display = "none";
+        document.querySelector('#loginForm').style.display = "none";
+    } else if(localStorage.login == "false" || localStorage.login == undefined){
+        console.log("not login: "+localStorage.login);
+        document.querySelector('#logedIn').style.display = "none";
+    }
+
+    $scope.login = function() {
+        const Http = new XMLHttpRequest();
+        const url='http://localhost:3000/login';
+        Http.open("POST", url, true);
+        Http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        Http.send("name="+document.querySelector('#name').value+
+                        "&pwd="+document.querySelector('#pwd').value);
+        Http.onreadystatechange= (e)=>{
+            console.log(Http);
+            if(Http.status == 200){
+                console.log("login");
+                localStorage.setItem("login", true);
+                localStorage.setItem("temp", Http.response);
+                document.querySelector('#logedIn').style.display = "block";
+                document.querySelector('.errorLogin').style.display = "none";
+                document.querySelector('#loginForm').style.display = "none";
+                window.location.reload();
+            } else{
+                localStorage.setItem("login", false);
+                localStorage.setItem("temp", undefined);
+                console.log("not login");
+                document.querySelector('#logedIn').style.display = "none";
+                document.querySelector('.errorLogin').innerHTML = (Http.status == 404) ? "The password is wrong": "The username is wrong";
+                document.querySelector('.errorLogin').style.display = "block";
+            }
+        };
+    };
+
+    $scope.logout = function() {
+        localStorage.setItem("login", false);
+        localStorage.setItem("temp", undefined);
+        document.querySelector('#logedIn').style.display = "none";
+        document.querySelector('.errorLogin').style.display = "none";
+        document.querySelector('#loginForm').style.display = "block";
+        window.location.reload();
+    };
+
+    $scope.register = function() {
+        document.querySelector('#registerModal').style.display = "block";
+    };
+
+});
