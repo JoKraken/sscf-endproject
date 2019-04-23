eb1060cd18622845d5a6fc73c9a2ebc58839c42e
JoKrakan <J.Kraken24@hotmail.com>
2019-04-23 21:04:55 +0300

    edit myReports html & controller

--- front/js/myReports.js
+++ front/js/myReports.js
@@ -1,14 +1,80 @@
 app.controller('myReportCtrl', function($scope) {
+    $scope.catoId = undefined;
 
-    fetch('/myReports/'+localStorage.temp, {
+    fetch('/items/'+$scope.catoId+'/'+localStorage.temp, {
         method: 'GET',
         headers:{
             'Content-Type': 'application/json'
         }
     }).then(res => res.json())
     .then((response) => {
+        console.log(response);
         $scope.reports = response;
-        $scope.$apply();
+        fetch('/category', {
+            method: 'GET',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then((response) => {
+                console.log(response);
+                $scope.categories = response;
+                $scope.$apply();
+            }).catch(error => console.error('Error:', error));
     }).catch(error => console.error('Error:', error));
-    
+
+    $scope.pressTitle = function (id) {
+        fetch('/items/'+id+'/'+localStorage.temp, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .then((response) => {
+                console.log(response);
+                $scope.reports = response;
+                $scope.$apply();
+            }).catch(error => console.error('Error:', error));
+    };
+
+    $scope.view = function (id) {
+        console.log(id);
+        $scope.reports.forEach(function (item) {
+            if (id == item._id) {
+                document.querySelector('.modal-body img').src = 'uploads/' + item.image;
+                document.querySelector('#titelView').innerHTML = item.title;
+                document.querySelector('#desView').innerHTML = item.details;
+                let date = new Date(item.time);
+
+                document.querySelector('#timeView').innerHTML = "("+date.getDate()+"."+date.getMonth()+"."+date.getFullYear()+") ";
+                document.querySelector('#myModal').style.display = "block";
+            }
+        });
+    };
+
+    $scope.edit = function (id) {
+        console.log(id);
+        $scope.reports.forEach(function (item) {
+            if (id == item._id) {
+                document.querySelector('#editForm').action = document.querySelector('#editForm').action + "?id=" + item._id;
+                console.log(document.querySelector('form').action);
+                document.querySelector('#cato').value = item.category;
+                document.querySelector('#title').value = item.title;
+                document.querySelector('#des').value = item.details;
+                document.querySelector('#editModal').style.display = "block";
+            }
+        });
+    };
+
+    $scope.delete = function (id) {
+        var url = '/item/'+id;
+
+        fetch(url, {
+            method: 'DELETE',
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(response => window.location.reload())
+            .catch(error => console.error('Error:', error));
+    }
 });
